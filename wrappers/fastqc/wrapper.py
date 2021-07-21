f9afd8fd7549e1535c864ed4bc73b6e9f41248cb
Thomas Cokelaer <cokelaer@gmail.com>
2021-07-21 17:36:31 +0200

    commit to follw PR comments

--- wrappers/fastqc/wrapper.py
+++ wrappers/fastqc/wrapper.py
@@ -11,32 +11,35 @@ from snakemake.shell import shell
 input_fastq = snakemake.input
 
 # figure out the expected output directory
-done = snakemake.input[0]
-outdir = done.rsplit("/")[0]
+done = snakemake.output[0]
+
+# figure out the output directory (just removing the filename.done)
+outdir = done.rsplit("/", 1)[0]
 
 params = snakemake.params
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+# we do not use this alias because we will concatenate possibly 2 output of
+# fastqc into a single log file for the paired data
+# log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+log = snakemake.log[0]
 
 
-# Note that if the input file is empty, fastqc will fail. 
-# We need to touch  a file in such case and your Snakefile
-# should define as output a "done" file.
+# Note that if the input file is empty, fastqc creates a HTML file
 
+# ( ͠° ͟ʖ ͡°) isinstance
 if isinstance(input_fastq, str):
     input_fastq = [input_fastq]
 else:
     input_fastq = input_fastq
 
-if len(input_fastq) != 0:
-    for fastq_file in input_fastq:
-        if fastq_file.endswith((".bam", "sam")):
-            shell(
-            " fastqc -t {snakemake.threads} --outdir {outdir} "
-                " {fast_file} {params.options} &>> {log}")
-        else:
-            shell(
-                " fastqc -t {snakemake.threads} --outdir {outdir} -f fastq "
-                " {fastq_file} {params.options} &>> {log}")
+for fastq_file in input_fastq:
+    if fastq_file.endswith((".bam", "sam")):
+        shell(
+        " fastqc -t {snakemake.threads} --outdir {outdir} "
+            " {fast_file} {params.options} &>> {log}")
+    else:
+        shell(
+            " fastqc -t {snakemake.threads} --outdir {outdir} -f fastq "
+            " {fastq_file} {params.options} &>> {log}")
 
-shell("touch({done})")
+shell("touch {done} ")
 
