556e751b4d97017f42c0e247a1ed07609b7f4f87
Thomas Cokelaer <cokelaer@gmail.com>
2021-07-21 17:11:54 +0200

    Fix workdir, log, touch

--- wrappers/fastqc/wrapper.py
+++ wrappers/fastqc/wrapper.py
@@ -9,8 +9,14 @@ from snakemake.shell import shell
 
 # Get rule information (input/output/params...)
 input_fastq = snakemake.input
+
+# figure out the expected output directory
+done = snakemake.input[0]
+outdir = done.rsplit("/")[0]
+
 params = snakemake.params
-log = snakemake.log[0]
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
 
 # Note that if the input file is empty, fastqc will fail. 
 # We need to touch  a file in such case and your Snakefile
@@ -22,13 +28,15 @@ else:
     input_fastq = input_fastq
 
 if len(input_fastq) != 0:
-    for this in input_fastq:
-        if this.endswith(".bam") or this.endswith("sam"):
+    for fastq_file in input_fastq:
+        if fastq_file.endswith((".bam", "sam")):
             shell(
-            " fastqc -t {snakemake.threads} --outdir {params.wkdir} "
-                " {input_fastq} {params.kargs} &> {log}")
+            " fastqc -t {snakemake.threads} --outdir {outdir} "
+                " {fast_file} {params.options} &>> {log}")
         else:
             shell(
-                " fastqc -t {snakemake.threads} --outdir {params.wkdir} -f fastq "
-                " {input_fastq} {params.kargs} &> {log}")
+                " fastqc -t {snakemake.threads} --outdir {outdir} -f fastq "
+                " {fastq_file} {params.options} &>> {log}")
+
+shell("touch({done})")
 
