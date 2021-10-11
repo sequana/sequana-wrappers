f8cfd2f441bae52c293f6ec0d0d58e06fba976d7
Thomas Cokelaer <cokelaer@gmail.com>
2021-10-11 11:42:19 +0200

    revert back

--- wrappers/multiqc/wrapper.py
+++ wrappers/multiqc/wrapper.py
@@ -9,7 +9,6 @@ from os import path
 
 from snakemake.shell import shell
 
-print("--------------------")
 output_dir = path.dirname(snakemake.output[0])
 output_name = path.basename(snakemake.output[0])
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
@@ -21,8 +20,6 @@ modules = snakemake.params.modules
 config_file = snakemake.params.config_file
 
 
-print(options)
-print(input_directory)
 # if config file not provided, should be set to empty string
 if config_file.strip():
     config_file = f" -c {config_file}"
