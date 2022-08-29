0f7083b43881c78af1a72255d5902d8dc1058589
ddesvillechabrol <d.desvillechabrol@gmail.com>
2022-08-29 16:16:10 +0200

    sambamba tmpdir is optional

--- wrappers/bwa/align/wrapper.py
+++ wrappers/bwa/align/wrapper.py
@@ -1,3 +1,15 @@
+#
+#  This file is part of Sequana software
+#
+#  Copyright (c) 2016-2021 - Sequana Dev Team (https://sequana.readthedocs.io)
+#
+#  Distributed under the terms of the 3-clause BSD license.
+#  The full license is in the LICENSE file, distributed with this software.
+#
+#  Website:       https://github.com/sequana/sequana
+#  Documentation: http://sequana.readthedocs.io
+#  Contributors:  https://github.com/sequana/sequana/graphs/contributors
+##############################################################################
 __author__ = "Thomas Cokelaer"
 __copyright__ = "Copyright 2021, Sequana Dev Team"
 __email__ = "thomas.cokelaer@pasteur.fr"
@@ -10,9 +22,9 @@ from shutil import which
 fastq = snakemake.input.fastq
 reference = snakemake.input.reference
 options = snakemake.params.get("options", "")
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+tmpdir = snakemake.params.get("tmp_directory", "")
 output_sorted_bam = snakemake.output.sorted
-params = snakemake.params
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 
 if which("pbwa"):
@@ -20,16 +32,13 @@ if which("pbwa"):
 else:
     bwa_exe = "bwa"
 
+if tmpdir:
+    sambamba_tmp = f"--tmpdir={tmpdir}"
 
-sambamba_sort = ""
 
-shell("""
-        ({bwa_exe} mem -t {snakemake.threads} {options} \
-        {reference} {fastq} | \
-        sambamba view -t {snakemake.threads} -S -f bam -o /dev/stdout /dev/stdin | \
-        sambamba sort /dev/stdin -o {output_sorted_bam} -t {snakemake.threads} \
-        --tmpdir={params.tmp_directory} """ + sambamba_sort + """ )  {log}
-"""
+shell(
+    "({bwa_exe} mem -t {snakemake.threads} {options} {reference} {fastq}"
+    " | sambamba view -t {snakemake.threads} -S -f bam -o /dev/stdout /dev/stdin"
+    " | sambamba sort /dev/stdin -o {output_sorted_bam} -t {snakemake.threads} {sambamba_tmp}"
+    ") {log}"
 )
-
-
