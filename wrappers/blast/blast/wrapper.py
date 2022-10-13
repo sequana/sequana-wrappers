b7d8c93ecada51ebfafeb235a3a4fecd2c26289e
ddesvillechabrol <d.desvillechabrol@gmail.com>
2022-10-13 15:26:31 +0200

    refacto blast

--- wrappers/blast/blast/wrapper.py
+++ wrappers/blast/blast/wrapper.py
@@ -1,28 +1,48 @@
-__author__ = "Antonie Vietor"
-__copyright__ = "Copyright 2021, Antonie Vietor"
-__email__ = "antonie.v@gmx.de"
-__license__ = "MIT"
+#
+#  This file is part of Sequana software
+#
+#  Copyright (c) 2016-2022 - Sequana Dev Team (https://sequana.readthedocs.io)
+#
+#  Distributed under the terms of the 3-clause BSD license.
+#  The full license is in the LICENSE file, distributed with this software.
+#
+#  Website:       https://github.com/sequana/sequana
+#  Documentation: http://sequana.readthedocs.io
+#  Contributors:  https://github.com/sequana/sequana/graphs/contributors
+##############################################################################
+from pathlib import Path
+from os import path
 
 from snakemake.shell import shell
-from os import path
+
+
+blastdb = snakemake.input.blastdb
+query = snakemake.input.query
+blast_type = snakemake.params.blast_type  # required params
+db_type = snakemake.params.get("db_type", "")
+outfmt = snakemake.params.get("outfmt")
+evalue = snakemake.params.get("evalue")
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
-format = snakemake.params.get("format", "")
-blastdb = snakemake.input.get("blastdb", "")[0]
-db_name = path.splitext(blastdb)[0]
+try:
+    blastdb = Path(blastdb)
+    if db_type:
+        blastdb = blastdb / db_type
+except TypeError:
+    # handle multiext case that create a list of input file
+    blastdb, _ = path.splitext(blastdb[0])
 
-blast_type = snakemake.params.get("blast_type", "")
-
-if format:
-    out_format = " -outfmt '{}'".format(format)
+outfmt = f"-outfmt '{outfmt}'" if outfmt else ""
+evalue = f"-evalue {evalue}" if evalue else ""
 
 shell(
     "{blast_type}"
-    " -query {snakemake.input.query}"
-    " {out_format}"
     " {snakemake.params.options}"
-    " -db {db_name}"
+    " -query {snakemake.input.query}"
+    " {outfmt}"
+    " {evalue}"
+    " -db {blastdb}"
     " -num_threads {snakemake.threads}"
-    " -out {snakemake.output[0]}"
+    " -out {snakemake.output} {log}"
 )
