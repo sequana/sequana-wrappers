043bb4262441fec490bfc16247ac9ddd649b030e
Thomas Cokelaer <cokelaer@gmail.com>
2026-03-21 14:12:18 +0100

    add macs3, mark_duplicates, sequana_coverage

--- sequana_wrappers/shells/bowtie2/build/v1/cmd.py
+++ sequana_wrappers/shells/bowtie2/build/v1/cmd.py
@@ -1,3 +1,6 @@
 CMD = """\
-bowtie2-build --threads {threads} {params.options} {input.reference} reference/genome > {log} 2>&1
+idx="{output[0]}"
+idx="${{idx%.1.bt2}}"
+idx="${{idx%.1.bt2l}}"
+bowtie2-build --threads {threads} {params.options} {input.reference} $idx > {log} 2>&1
 """
