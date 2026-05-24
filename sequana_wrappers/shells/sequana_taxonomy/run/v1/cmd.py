be194fb4310bf2cbf21b643c4af89301b7529aef
Thomas Cokelaer <cokelaer@gmail.com>
2026-05-24 22:28:45 +0200

    Implement unclassified reads extraction in sequana_taxonomy
    Extract unclassified reads from kraken output instead of creating empty file.
    Handles both paired-end and single-end input. Cleanup temp files after merging.

--- sequana_wrappers/shells/sequana_taxonomy/run/v1/cmd.py
+++ sequana_wrappers/shells/sequana_taxonomy/run/v1/cmd.py
@@ -13,6 +13,17 @@ cmd="sequana_taxonomy $input_opt \
     {params.options}"
 eval "$cmd" > {log} 2>&1
 if [ "{params.store_unclassified}" = "True" ]; then
-    touch $outdir/unclassified.fastq
+    awk '$1=="U"{{print $2}}' $outdir/kraken/kraken.out > $outdir/kraken/unclassified_ids.txt
+    if [ ${{#fastq_files[@]}} -eq 2 ]; then
+        seqkit grep -f $outdir/kraken/unclassified_ids.txt ${{fastq_files[0]}} > $outdir/kraken/_unclassified_R1.fastq 2>> {log}
+        seqkit grep -f $outdir/kraken/unclassified_ids.txt ${{fastq_files[1]}} > $outdir/kraken/_unclassified_R2.fastq 2>> {log}
+        cat $outdir/kraken/_unclassified_R1.fastq $outdir/kraken/_unclassified_R2.fastq > $outdir/kraken/unclassified.fastq
+        rm -f $outdir/kraken/_unclassified_R1.fastq $outdir/kraken/_unclassified_R2.fastq
+    else
+        seqkit grep -f $outdir/kraken/unclassified_ids.txt ${{fastq_files[0]}} > $outdir/kraken/unclassified.fastq 2>> {log}
+    fi
+    rm -f $outdir/kraken/unclassified_ids.txt
+else
+    touch $outdir/kraken/unclassified.fastq
 fi
 """
